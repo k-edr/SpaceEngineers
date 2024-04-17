c6642f6189139582a062bf3df227d0d141a9d6c7
k-edr <yevhen.puzenko@gmail.com>
2024-04-17 23:16:26 +0300

    lidar

--- LidarRadar/Program.cs
+++ LidarRadar/Program.cs
@@ -2,27 +2,95 @@
 using System.Collections.Generic;
 using System.Linq;
 using IngameScript.Const;
+using IngameScript.Custom;
 using IngameScript.Logger;
 using IngameScript.MyGridSystem;
 using Sandbox.ModAPI.Ingame;
+using VRageMath;
 
 namespace IngameScript
 {
-    partial class Program : MyGridProgram
-    {      
-        public Program()
+    
+    class Lidar
+    {
+        private List<IMyCameraBlock> cameras = null;
+
+        private int _scanDistanceLimit = 0;
+
+        private IEnumerator<IMyCameraBlock> _enumerator;
+        public Lidar(IReadOnlyCollection<IMyCameraBlock> cameras, int scanDistanceLimit)
         {
-            StuffInit();
+            this.cameras = cameras.ToList();
+
+            _scanDistanceLimit = scanDistanceLimit;
+
+            _enumerator = cameras.GetEnumerator();
+
+            EnableRaycast(this.cameras);
         }
-     
-        public void Save()
+
+        private void EnableRaycast(ICollection<IMyCameraBlock> collection)
+        {
+            foreach (var camera in collection)
+            {
+                camera.EnableRaycast = true;
+            }
+        }
+        public MyDetectedEntityInfo Scan(float pitch = 0, float yaw = 0)
+        {
+            IMyCameraBlock camera = _enumerator.Current;
+
+            if (_enumerator.MoveNext())
+            {
+                _enumerator.Reset();
+            }
+
+            return camera.Raycast(_scanDistanceLimit, pitch, yaw);
+        }
+    }
+   
+    partial class Program : MyGridProgram, ITemplate
+    {
+        public void Init()
         {
             
         }
-      
+
+        public bool tryLock = false;
         public void Execute(string argument, UpdateType updateSource)
         {
-        
-        }   
+            if(updateSource == UpdateType.Trigger || updateSource == UpdateType.Terminal)
+            {
+                switch (argument)
+                {
+                    case "TryLock":
+
+                        tryLock = true;
+
+                        break;
+                    case "StopLock":
+
+                        tryLock = false;
+
+                        break;
+                    default:
+
+                        Echo("Unknown argument: " + argument);
+
+                        Logger.AddLine("Unknown argument: " + argument);
+
+                        return;
+                }
+
+                Logger.AddLine("Executed command: " + argument);
+            }
+            else
+            {
+                if (tryLock)
+                {
+
+                }
+            }      
+        }
     }
 }

b899cbeecce859987476947be61a272067614352
k-edr <yevhen.puzenko@gmail.com>
2024-05-20 20:54:22 +0200

    init

---
+++ AIB.Core.Statistica/Class1.cs
@@ -0,0 +1,32 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using VRage;
+using VRage.Collections;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRageMath;
+
+
+//items, power_using, ammo_using
+namespace IngameScript
+{
+    partial class Program
+    {
+        public class Class1
+        {
+
+        }
+    }
+}

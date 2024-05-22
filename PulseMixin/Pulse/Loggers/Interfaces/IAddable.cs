e3220dac920172b0390747ba816a08ca2f92426c
k-edr <yevhen.puzenko@gmail.com>
2024-05-22 22:54:20 +0200

    /

--- PulseMixin/Pulse/Loggers/Interfaces/IAddable.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace IngameScript.IngameScript.Pulse.Loggers.Interfaces
-{
-    public interface IAddable
-    {
-        void Add(string message, object data = null);
-
-        void AddLine(string message, object data = null);
-    }
-}
